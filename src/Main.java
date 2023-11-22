7a0c8c51b7ab080e9ee840b91a799d2137fdab20
MaksymilianOE <oema1103@hss-wi.de>
2023-11-22 13:29:14 +0100

    TickTackToe Basic

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int rows = 3; //X
+        int column = 3; //Y
+        /**
+         * - - -
+         * - X - => 2,2
+         * - - -
+         */
+        try {
+            setPoint(3,2, "X");
+        } catch (Exception e) {
+            System.out.println("Fehler! " + e);
+        }
+    }
+
+    public static void setPoint(int X, int Y, String type) throws Exception {
+        if (X <= 3 && Y <= 3) {
+            render(X,Y, type);
+        } else {
+            throw new Exception("Die Zahlen sind nicht im Rahmen des Spiels (max 3)");
+        }
+    }
+
+    public static void render(int row, int column, String icon) {
+        for (int i = 1; i <= 3; i++) { //Row
+            for (int j = 1; j <= 3; j++) {
+                if (i == row && j == column) {
+                    System.out.print(" " + icon + " ");
+                } else {
+                    System.out.print(" - ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
